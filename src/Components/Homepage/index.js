1d2feba595f1419f028f6b466b7cee2046431d57
Joshua Almodovar <joshuarosamktp4@gmail.com>
2022-08-05 23:23:15 -0500

    Add text to hero

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,16 +1,17 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import heroBackground from "../../Assets/Images/background.jpg";
-// import { Container } from "react-bootstrap";
 
 function Homepage() {
   return (
-    <div>
-      <div>
-        <img
-          className="heroBG"
-          src={heroBackground}
-          alt="Picture of a cars steering wheel"
-        ></img>
+    <div className="heroContainer">
+      <img
+        className="heroBG"
+        src={heroBackground}
+        alt="A cars steering wheel"
+      ></img>
+      <div className="heroText">
+        <p>TESTING TESTING</p>
       </div>
     </div>
   );
