d005379b9ecc567b55fa9232c6d8b14201f594c3
Joshua Almodovar <joshuarosamktp4@gmail.com>
2022-08-15 09:51:34 -0500

    Start gallery Page

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Gallery () {
 
 return (
+    <main className="galleryPage">
         <div>
-            <h1>Hello page!</h1>
+            <Container fluid>
+                <Row>
+                    <Col xs={6} md={4}>
+                        <h1 className="galleryTitle">Photo Gallery</h1>
+                        <br></br>
+                    </Col>
+                </Row>
+            </Container>
         </div>
+        <div>
+
+        </div>
+    </main>
     );
 }
 
